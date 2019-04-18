1f373b9790c5c38298197651df542435450dc782
Gus Cost <guscost@gmail.com>
2019-04-18 19:41:26 -0400

    gus: completed reDealCardRows function in 21-card-trick

--- guscost/21-card-trick/index.js
+++ guscost/21-card-trick/index.js
@@ -96,5 +96,12 @@ function reDealCardRows(cardRows, selectedRowIndex) {
     ...cardRows[1]
   ];
 
-  
+  const newRows = [[], [], []];
+  for (let i = 0; i < 21; i++) {
+    newRows[i % 3].push(pileOfCards[i]);
+  }
+
+  return newRows;
 }
+
+
