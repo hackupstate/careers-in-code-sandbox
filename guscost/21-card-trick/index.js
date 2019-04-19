4de3aaffe3b2052d88d454ec8e3322e5a4b62611
Gus Cost <guscost@gmail.com>
2019-04-18 21:24:50 -0400

    gus: add comments to second half of 21-card-trick

--- guscost/21-card-trick/index.js
+++ guscost/21-card-trick/index.js
@@ -108,43 +108,66 @@ function promptForRowIndex() {
   return rowNumber - 1;
 }
 
+// Place the selected row between the other two rows and re-deal
 function reDealCardRows(cardRows, selectedRowIndex) {
+
+  // Splice out just the selected row (this *mutates* cardRows!)
+  // Mutating arguments is generally bad, but we'll allow it here
   const selectedRow = cardRows.splice(selectedRowIndex, 1)[0];
 
+  // Move the selected row "in between" the other two rows
+  // Also flatten all three rows into one pile with the ... ("spread") operator
+  // (cardRows only has two items in it, since we removed the selected one!)
   const pileOfCards = [
     ...cardRows[0],
     ...selectedRow,
     ...cardRows[1]
   ];
 
+  // "Deal" this pile into three new rows
   const newRows = [[], [], []];
   for (let i = 0; i < 21; i++) {
+    // We find the correct row with the "modulo" operator
+    // Here this is exactly like finding the *remainder* of i / 3
+    // But modulo isn't always the same as remainder (try negative numbers!)
     newRows[i % 3].push(pileOfCards[i]);
   }
 
+  // Return the three new rows in our new array-of-arrays
   return newRows;
 }
 
+// Do the 21-card trick all the way through
 function doTheTrick() {
 
+  // Initialize our three rows of random cards
   let cardRows = initializeCardRows();
-  console.log('Pick a card, and remember it.')
+  console.log('Pick a card, and remember it.');
 
+  // Always loop at least twice: render, prompt, and re-deal
   for(let i = 0; i < 2; i++) {
     renderCardRows(cardRows);
+    // This is block-scoped to only one loop, so we can use const!
     const selectedRowIndex = promptForRowIndex();
     cardRows = reDealCardRows(cardRows, selectedRowIndex);
   }
 
+  // We updated the selected row *after* rendering, but we need to check it
+  // *before* rendering, so initialize it (as mutable!) outside the loop
   let selectedRow;
+
+  // As long as the user hasn't chosen "Row 2" (index 1!) we need to keep
+  // looping exactly like before, but we're done as soon as they select "Row 2"
   while (selectedRow !== 1) {
     renderCardRows(cardRows);
     selectedRow = promptForRowIndex();
     cardRows = reDealCardRows(cardRows, selectedRow);
   }
 
+  // The user's card is the middle card (index 3) of the middle row (index 1)
   const userCard = cardRows[1][3];
   console.log(`Your card is: ${cardToSymbol(userCard)}`);
 }
 
+// Do the trick!
 doTheTrick();
