8bbe5b3333b9c0bd86660f579a4b21dc954ecdf7
karin thorne <40842605+kethorne@users.noreply.github.com>
2019-04-18 20:02:25 -0400

    copied gus's code

--- kthorne/21-card-trick/index.js
+++ kthorne/21-card-trick/index.js
@@ -87,4 +87,14 @@ function promptForRowIndex() {
   return rowNumber - 1;
 }
 
-console.log(renderCardRows(dealCardRows));
+function reDealCardRows(cardRows, selectedRowIndex) {
+  const selectedRow = cardRows.splice(selectedRowIndex, 1)[0];
+
+  const pileOfCards = [
+    ...cardRows[0],
+    ...selectedRow,
+    ...cardRows[1]
+  ];
+
+  
+}
