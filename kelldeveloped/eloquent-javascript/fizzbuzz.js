eaafbcad1f892579aa719713dc45bbb0b8269c2b
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-04-21 15:40:58 -0400

    Answered exercises for homework from Eloquent JavaScript chapters 2 and 3

---
+++ kelldeveloped/eloquent-javascript/fizzbuzz.js
@@ -0,0 +1,18 @@
+/* Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those). */
+
+/* let numberLog = console.log(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100); */
+
+for (let start = 0; start <=100; start = start + 1) {
+    if (start % 3 && start % 5 === 0 ) {
+        console.log("fizzbuzz");
+    } else if (start % 5 === 0) {
+        console.log("buzz");
+    } else if (start % 3 === 0) {
+        console.log("fizz")
+    } else {
+        console.log(start);
+    }
+}
+
