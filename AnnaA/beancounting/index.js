1eeb29f786a914c434a7e9653d81eec05d604f5b
Anna <48394404+World-Cup@users.noreply.github.com>
2019-04-22 13:56:32 -0400

    second commit

--- AnnaA/beancounting/index.js
+++ AnnaA/beancounting/index.js
@@ -1,7 +1,10 @@
 function countBs(str) {
+    //initialize caseCount to zero
     var caseCount = 0;
+    /* sets i to 0, checks condition, increment i */
     for (var i = 0; i < str.length; i ++){
         if (str.charAt(i) === "B")
+        //checks if letter @ index position i is B
             caseCount ++;
     }
     return caseCount
