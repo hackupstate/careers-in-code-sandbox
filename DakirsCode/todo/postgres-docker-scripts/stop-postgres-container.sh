8144a43997cad3413fd5e314423ef6df1c89e926
Alice Miller <alice_miller@weakmint.dev>
2019-06-19 12:36:28 -0400

    Use BASH_SOURCE to find vars file localtion

--- DakirsCode/todo/postgres-docker-scripts/stop-postgres-container.sh
+++ DakirsCode/todo/postgres-docker-scripts/stop-postgres-container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-source ./vars.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/vars.sh
 docker stop $NAME
--- amiller/start-postgres-container.sh
+++ amiller/start-postgres-container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source ./vars.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/vars.sh
 docker run -d --rm --name $NAME \
 -e POSTGRES_PASSWORD=cic -p 5432:5432 \
 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data \
