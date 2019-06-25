34e62138b36e50d53b78bbebcc98ba4559157ae2
Max Matthews <max@mzmtech.com>
2019-06-24 20:30:56 -0400

    Set up to do app models and sequelize

---
+++ MaxMatthews/todo/models.js
@@ -0,0 +1,34 @@
+module.exports = () => {
+  const Sequelize = require("sequelize");
+  const db = new Sequelize("postgres", "postgres", "cic", {
+    host: "127.0.0.1",
+    dialect: "postgres"
+  });
+
+  return {
+    db,
+    User: db.define("User", {
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: Sequelize.STRING
+    }),
+    Todo: db.define("Todo", {
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: Sequelize.STRING,
+      createdAt: Sequelize.DATE,
+      completetionTime: Sequelize.DATE,
+      dueTime: Sequelize.DATE,
+      userID: Sequelize.INTEGER.UNSIGNED
+    }),
+    init: () => {
+      db.sync({ force: true });
+    }
+  };
+};
---
+++ MaxMatthews/todo/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// const Sequelize = require("sequelize");
+const models = require("./models")();
+models.init();
+
+const bodyparser = require("body-parser");
+
+app.use(bodyparser.json());
+
+app.get("/", (req, res) => {
+  res.send({ hello: "world" });
+});
+
+app.listen(8080, "0.0.0.0", () => {
+  console.log("Server is running");
+});
