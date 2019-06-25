df0c0512b2f2a0ac05fa4ba3a23a90efb637fcbe
glitchCreativeLabs <34613727+GlitchLabs@users.noreply.github.com>
2019-06-25 18:10:17 -0400

    adding my todo list app progress from day one with make we did the server and the database

---
+++ DakirsCode/todo/ model.js
@@ -0,0 +1,41 @@
+module.exports = ( ) => {
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres","postgres","cic",
+{
+  host:"127.0.0.1",
+  dialect:"postgres",
+  pool:
+  {
+    max:5,
+    min:0,
+    idle:10000
+  }
+});
+
+return {
+    db,
+    User: db.define("User", {
+        id: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: Sequelize.STRING
+    }),
+    Todo: db.define("Todo",{
+        id: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: Sequelize.STRING,
+        createdAt: Sequelize.DATE,
+        completationTime: Sequelize.DATE,
+        dueTime: Sequelize.DATE,
+        userId: Sequelize.INTEGER.UNSIGNED
+    }),//end of object
+    init: () => {
+    db.sync();
+       }
+    }//end of return
+}//end of export
