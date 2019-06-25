3704fcef7b51091879f22f5b9f4ed1396e3c33ca
Max Matthews <max@mzmtech.com>
2019-06-25 11:00:30 -0400

    Add comments

--- MaxMatthews/todo/models.js
+++ MaxMatthews/todo/models.js
@@ -1,19 +1,31 @@
+//put all the code in a module.exports so we can access it in another file using require
 module.exports = () => {
+  //import sequelize like any other npm package
   const Sequelize = require("sequelize");
+  //open the connection to the DB using a new sequelize instance
+  //the parameters are: database, username, password, host, and dialect (postgres, mysql, mssql, etc)
   const db = new Sequelize("postgres", "postgres", "cic", {
     host: "127.0.0.1",
     dialect: "postgres"
   });
 
+  //anything returned in out module.exports will be accessible in other files
   return {
-    db,
+    db, //we don't use this yet, but we may need access to the raw DB connection down the road
+    /*
+      Define (using the Sequelize DB connection) a User model. User is both the model 
+      object name (before the colon) AND the table name in the database 
+      (the first param of the define method). We then pass it the fields (columns) that our model
+      (and table in Postgres) will have with their types.
+    */
     User: db.define("User", {
       id: {
+        //we use ID on almost every model/table. It makes it easier to create keys down the road
         type: Sequelize.INTEGER.UNSIGNED,
-        autoIncrement: true,
+        autoIncrement: true, //we use autoIncrement to tell postgre that it can create our IDs for us
         primaryKey: true
       },
-      name: Sequelize.STRING
+      name: Sequelize.STRING //we don't pass an object here when defining name because we only need the type
     }),
     Todo: db.define("Todo", {
       id: {
@@ -25,10 +37,11 @@ module.exports = () => {
       createdAt: Sequelize.DATE,
       completetionTime: Sequelize.DATE,
       dueTime: Sequelize.DATE,
-      userID: Sequelize.INTEGER.UNSIGNED
+      userID: Sequelize.INTEGER.UNSIGNED //unsigned means it will be a non negative (positive) number
     }),
     init: () => {
-      db.sync({ force: true });
+      //this code won't run until we call init in another file.
+      db.sync({ force: true }); //sync to copy our model structure to the database
     }
   };
 };
--- MaxMatthews/todo/server.js
+++ MaxMatthews/todo/server.js
@@ -1,18 +1,26 @@
+//import express from npm
 const express = require("express");
-const app = express();
+const app = express(); //set up a server instance of express
 
-// const Sequelize = require("sequelize");
+//import our models from our file
+//because the require paramater is a file path, it means use our own file
+//instead of an npm package
 const models = require("./models")();
+
+//call the init function which in the models.js file, syncs our models with the DB
 models.init();
 
+//bodyparser so we can have access to the req.body object when we send JSON in a request
 const bodyparser = require("body-parser");
-
 app.use(bodyparser.json());
 
+//get request to the home page and send back some JSON
 app.get("/", (req, res) => {
   res.send({ hello: "world" });
 });
 
+//tell the server to listen on port 8080 on any API address
 app.listen(8080, "0.0.0.0", () => {
+  //add a callback so we know there were no errors turning on
   console.log("Server is running");
 });
