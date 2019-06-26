62f87e4504edb05cdfdc1bf6039079bfa3734aeb
Max Matthews <max@mzmtech.com>
2019-06-26 10:15:45 -0400

    Added comments

--- MaxMatthews/todo/index.js
+++ MaxMatthews/todo/index.js
@@ -23,21 +23,26 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+//post endpoint for creating new task
 app.post("/task", async (req, res) => {
+  //use the sequelize model to create a todo
   await models.Todo.create({
-    name: req.body.name,
-    createdAt: new Date(),
+    name: req.body.name, //req.body is our body from our fetch request
+    createdAt: new Date(), //system generated timestamp
     dueTime: req.body.dueTime,
     userName: req.body.userName
   });
 
+  //every endpoint needs to send something back to the server
   res.send({ message: "ok" });
 });
 
 app.get("/task", async (req, res) => {
+  //go to the DB using the model, get all the tasks...
   const tasks = await models.Todo.findAll();
 
-  res.send({ tasks });
+  /////then send them back using JSON
+  res.send({ tasks }); //note, {tasks} is the equivalent of {tasks: tasks}
 });
 
 //tell the server to listen on port 8080 on any IP address
