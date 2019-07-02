1469743796a0506e0452c280635e549e2078835d
lltim <48389438+lltim@users.noreply.github.com>
2019-07-02 17:33:11 -0400

    updated code so that app is running

--- timLiles/feedback/client/main.jsx
+++ timLiles/feedback/client/main.jsx
@@ -1,8 +1,3 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import App from '/imports/ui/App'
-
-Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
-});
+import { Meteor } from "meteor/meteor";
+import Feedback from "../imports/api/feedback.js"
+Meteor.startup(() => {});
