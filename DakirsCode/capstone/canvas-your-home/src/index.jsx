48d5727c1b858a8cf025256c4521388b8752b432
glitchCreativeLabs <34613727+GlitchLabs@users.noreply.github.com>
2019-07-22 18:04:22 -0400

    capstone folder for git hub

---
+++ DakirsCode/capstone/canvas-your-home/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
+import Counter from "./react-crash-course-components/Counter";
+
+const app = document.getElementById("root");
+ReactDOM.render(<Counter />, app);
