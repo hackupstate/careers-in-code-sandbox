08296b6e8ce67bf9b0a37d0d3e6763adf3171f2b
Gus Cost <guscost@gmail.com>
2019-08-18 12:54:43 -0400

    code: messages function

--- guscost/chatroom-firebase/functions/src/index.ts
+++ guscost/chatroom-firebase/functions/src/index.ts
@@ -1,8 +1,18 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+export const messages = functions.https.onRequest(async (req, res) => {
+  try {
+    console.log('REQUEST METHOD: ' + req.method);
+    const records = await db.collection('messages').get(); // .where('text', '==', 'hello')
+    return res.send(records.docs);
+  } catch (error) {
+    console.log("Error getting documents: ", error);
+    return res.send(error);
+  }
+});
+
+
