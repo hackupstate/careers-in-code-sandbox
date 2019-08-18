76c0f38bf1f7ad7df2796b209c2f40835f49966b
Gus Cost <guscost@gmail.com>
2019-08-18 13:14:01 -0400

    code: nicer document return data

--- guscost/chatroom-firebase/functions/src/index.ts
+++ guscost/chatroom-firebase/functions/src/index.ts
@@ -9,10 +9,10 @@ export const messages = functions.https.onRequest(async (req, res) => {
     const collection = db.collection('messages');
     if (req.method === 'GET') {
       const query = await collection.get(); // .where('text', '==', 'hello')
-      return res.send(query.docs);
+      return res.send(query.docs.map(doc => doc.data));
     } else if (req.method === 'POST') {
-      const query = await collection.add(req.body);
-      return res.send(query);
+      await collection.add(req.body);
+      return res.sendStatus(200);
     } else {
       return res.sendStatus(405);
     }
