1cb226e38d32ac66491ed7afba7e7c8f6c2823f5
Gus Cost <guscost@gmail.com>
2019-08-18 15:08:02 -0400

    code: post messages in the react app

--- guscost/chatroom-firebase/client/src/App.tsx
+++ guscost/chatroom-firebase/client/src/App.tsx
@@ -1,31 +1,69 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  const [messages, setMessages] = useState([] as Array<any>);
-  useEffect(() => {
-    const gusMessagesUrl = 'https://us-central1-chatroom-6a5a0.cloudfunctions.net/messages';
-    fetch(gusMessagesUrl).then(res => res.json()).then(data => {
-      setMessages(data);
-    });
+const MESSAGES_URL = 'https://us-central1-chatroom-6a5a0.cloudfunctions.net/messages';
+
+// Helper function to fetch all messages
+// The setMessages argument is a function!
+const fetchMessages = (setMessages: Function) => {
+  return fetch(MESSAGES_URL).then(res => res.json()).then(data => {
+    setMessages(data);
   });
+};
+
+// Helper function to create a new message
+// We could automatically call setMessages here if we passed it in
+// But then we'd also need to pass in all the existing messages!
+const postMessage = (text: string) => {
+  return fetch(MESSAGES_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ text })
+  } as any);
+};
+
+const App: React.FC = () => {
+
+  // State for the message input box
+  const [newMessage, setNewMessage] = useState('');
+
+  // State for the list of messages
+  const [messages, setMessages] = useState([] as Array<any>);
+  
+  // Fetch the messages on first load (don't forget the empty array)
+  // The function can't return anything, so we should use curly braces
+  useEffect(() => { fetchMessages(setMessages); }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type="text"
+          placeholder="Add message..."
+          value={newMessage}
+          onChange={e => setNewMessage(e.target.value)}
+        />
+        <button
+          onClick={async () => {
+            // Only save if there actually is a message
+            if (newMessage !== '') {
+              // Because we're awaiting, we don't want the UI to lag
+              // So, clear out the text box before any API calls
+              setNewMessage('');
+
+              // Do the POST to /messages
+              await postMessage(newMessage);
+
+              // EITHER: Fetch the list of messages again
+              await fetchMessages(setMessages);
+              // // OR: Optimize by updating the list of messages with the new one
+              // // The server could respond to the POST with the new message's id
+              // // Because this is a lot harder to do correctly, don't bother
+              // setMessages([...messages, { id: ???, text: newMessage }]);
+            }
+          }}
+        >Add message</button>
+      </div>
       <ul>
         {messages.map(
           // This "any" function parameter typing is redundant
