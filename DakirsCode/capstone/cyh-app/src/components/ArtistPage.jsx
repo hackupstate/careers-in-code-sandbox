dabc8ca3842df3577e154ca76c6ce9f41226f176
glitchCreativeLabs <34613727+GlitchLabs@users.noreply.github.com>
2019-08-22 07:35:41 -0400

    my slides

--- DakirsCode/capstone/cyh-app/src/components/ArtistPage.jsx
+++ DakirsCode/capstone/cyh-app/src/components/ArtistPage.jsx
@@ -1,37 +1,35 @@
 import React from "react";
 import {Container, Row, Col} from "reactstrap";
 
-export default class ArtistPage extends React.Component {
-	state = {products: []};
-
-	render() {
-		return (
-			<Container>
-				<Row>
-					<Col>
-						<div className="text-center">
-							<h1>The Artist-Rashad Mustafa</h1>
-						</div>
-					</Col>
-				</Row>
-				<Row>
-					<Col md={6}>
-						<p>this is a paragraph about the artist</p>
-					</Col>
-					<Col md={6}>
-						{this.state.products.map(product => {
-							return (
-								<div className="canvas">
-									<img
-										src="https://s3.us-east-2.amazonaws.com/canvasyourhome.art/images/the+artist.jpg"
-										alt="artwork"
-									/>
-								</div>
-							);
-						})}
-					</Col>
-				</Row>
-			</Container>
-		);
-	}
-}
+const ArtistPage = () => {
+	return (
+		<div id="artist-L1-main">
+			<div id="artist-L2-artist-info">
+				<div id="artist-L3-pic">
+					{/* <img
+						id="artist-pic"
+						src="https://s3.us-east-2.amazonaws.com/canvasyourhome.art/images/the+artist.jpg"
+						alt=""
+					/> */}
+				</div>
+				<div id="artist-L3-contact">
+					<h1>Rashad Mustafa Artist , Founder And Ceo</h1>
+					<p>email</p>
+					<p>phone number</p>
+					<p>
+						Born in Syracuse New York I found a love for art at a young age.
+						started sketching my favorite characters. I then started taking as
+						many art classes as I could while in school. After school I stopped
+						for a while just trying to pay off student loans. Then I had my son
+						and I found myself with some free time as he was getting older. we
+						would hang in my man cave for hours. then I decided to decorate.
+						after seeing how much posters and pictures cost I decided to
+						decorate with my own art
+					</p>
+				</div>
+			</div>
+			{/* <div id="artist-L2-bio" className="text-center" /> */}
+		</div>
+	);
+};
+export default ArtistPage;
